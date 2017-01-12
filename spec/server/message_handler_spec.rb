9fa03ecdabc1cb8561660eae78a37fc994dba08c
Dominik <dominik.malantowicz@gmail.com>
2017-01-12 22:01:51 +0100

    Add regression tests for MessageHandler call method

--- spec/server/message_handler_spec.rb
+++ spec/server/message_handler_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
-describe Server::MessageHandler do
+describe Server::MessageHandler do using RSpec::Parameterized::TableSyntax
 
+  where(:input, :output) do
+    'User' | '+OK User accepted, password please'
+    'user ' | '+OK User accepted, password please'
+    'USER ' | '+OK User accepted, password please'
+    "User\n" | '+OK User accepted, password please'
+    "USER\r" | '+OK User accepted, password please'
+    "uSER\r\n" | '+OK User accepted, password please'
+    "\r\nuser" | '+OK User accepted, password please'
+    ' user' | '+OK User accepted, password please'
+    "uSER\r\n\r something" | '+OK User accepted, password please'
+  end
+
+  with_them do
+    it 'should works correctly' do
+      expect(described_class.new(input).call).to eq(output), "Fail on input '#{input}'"
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'server'
 require 'timecop'
 require 'byebug'
+require 'rspec-parameterized'
