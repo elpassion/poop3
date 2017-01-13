420d9cbe031b63a2773dcbf256d6a1014a2d43de
Rafał Lisowski <lisukorin@gmail.com>
2017-01-13 11:31:07 +0100

    Test STAT command

---
+++ spec/server/commands/stat_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Server::MessageHandler do
+  using RSpec::Parameterized::TableSyntax
+
+  subject { described_class.new(input).call }
+
+  context "given STAT command" do
+    context "with valid parameter" do
+      where(:input, :output) do
+        'STAT' | '+OK 2 320'
+      end
+
+      with_them do
+        it 'returns success message' do
+          expect(subject).to eq(output), "Fail on input '#{input}'"
+        end
+      end
+    end
+  end
+end
