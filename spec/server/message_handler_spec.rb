513d25a46c00ac8c3436f8cade0e614ffd9e97a6
Karol Sarnacki <karol.sarnacki@elpassion.com>
2017-01-13 10:30:31 +0100

    [Refactoring] Separate tests for valid and invalid input

--- spec/server/message_handler_spec.rb
+++ spec/server/message_handler_spec.rb
@@ -1,24 +1,40 @@
 require 'spec_helper'
 
-describe Server::MessageHandler do using RSpec::Parameterized::TableSyntax
+describe Server::MessageHandler do
+  using RSpec::Parameterized::TableSyntax
 
-  where(:input, :output) do
-    'User' | '+OK User accepted, password please'
-    'user ' | '+OK User accepted, password please'
-    'USER ' | '+OK User accepted, password please'
-    "User\n" | '+OK User accepted, password please'
-    "USER\r" | '+OK User accepted, password please'
-    "uSER\r\n" | '+OK User accepted, password please'
-    "\r\nuser" | '+OK User accepted, password please'
-    ' user' | '+OK User accepted, password please'
-    "uSER\r\n\r something" | '+OK User accepted, password please'
-    'Wrong command' | 'Unknown command'
-    'very bad command' | 'Unknown command'
+  subject { described_class.new(input).call }
+
+  context "given valid command" do
+    where(:input, :output) do
+      'User' | '+OK User accepted, password please'
+      'user ' | '+OK User accepted, password please'
+      'USER ' | '+OK User accepted, password please'
+      "User\n" | '+OK User accepted, password please'
+      "USER\r" | '+OK User accepted, password please'
+      "uSER\r\n" | '+OK User accepted, password please'
+      "\r\nuser" | '+OK User accepted, password please'
+      ' user' | '+OK User accepted, password please'
+      "uSER\r\n\r something" | '+OK User accepted, password please'
+    end
+
+    with_them do
+      it 'returns valid response' do
+        expect(subject).to eq(output), "Fail on input '#{input}'"
+      end
+    end
   end
 
-  with_them do
-    it 'should work correctly' do
-      expect(described_class.new(input).call).to eq(output), "Fail on input '#{input}'"
+  context "given invalid command" do
+    where(:input, :output) do
+      'Wrong command' | 'Unknown command'
+      'very bad command' | 'Unknown command'
+    end
+
+    with_them do
+      it 'returns unknown command message' do
+        expect(subject).to eq(output), "Fail on input '#{input}'"
+      end
     end
   end
 end
